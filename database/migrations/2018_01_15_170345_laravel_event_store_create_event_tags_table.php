60d58b50245330914151af03c65cdd5c2e523478
Rasmus Nielsen <rasmus@easify.dk>
2018-01-17 12:22:38 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/2018_01_15_170345_laravel_event_store_create_event_tags_table.php
+++ database/migrations/2018_01_15_170345_laravel_event_store_create_event_tags_table.php
@@ -16,12 +16,12 @@ class LaravelEventStoreCreateEventTagsTable extends Migration
         Schema::create('event_tags', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('event_id')->unsigned();
-            $table->string("related_id");
-            $table->string("related_type");
+            $table->string('related_id');
+            $table->string('related_type');
             $table->string('name')->nullable();
             $table->timestamps();
 
-            $table->index(["related_id", "related_type"]);
+            $table->index(['related_id', 'related_type']);
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade')->onUpdate('cascade');
         });
     }
